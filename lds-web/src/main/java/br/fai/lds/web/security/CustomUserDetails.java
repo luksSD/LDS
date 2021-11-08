82149444d2f1ff9e6d054ff46e97aeacf04c255a
Lucas Dias <lucas.sd94@gmail.com>
2021-11-08 18:55:34 -0300

    aula 3-11 implementada

--- lds-web/src/main/java/br/fai/lds/web/security/CustomUserDetails.java
+++ lds-web/src/main/java/br/fai/lds/web/security/CustomUserDetails.java
@@ -1,5 +1,27 @@
 package br.fai.lds.web.security;
 
-public class CustomUserDetails {
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+
+import br.fai.lds.model.Usuario;
+
+public class CustomUserDetails extends User {
+
+	public CustomUserDetails(final String username, final String password,
+			final Collection<? extends GrantedAuthority> authorities, final Usuario user) {
+
+		super(username, password, true, true, true, true, authorities);
+
+		this.user = user;
+
+	}
+
+	private final Usuario user;
+
+	public Usuario getUsuario() {
+		return user;
+	}
 
 }
--- lds-web/src/main/java/br/fai/lds/web/security/provider/LdsAuthenticationProvider.java
+++ lds-web/src/main/java/br/fai/lds/web/security/provider/LdsAuthenticationProvider.java
@@ -10,9 +10,12 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
 import br.fai.lds.model.Usuario;
+import br.fai.lds.web.security.CustomUserDetails;
 import br.fai.lds.web.service.UserService;
 
 @Component
@@ -36,15 +39,26 @@ public class LdsAuthenticationProvider implements AuthenticationProvider {
 		}
 
 		final List<GrantedAuthority> grantedAuthorityList = new ArrayList<GrantedAuthority>();
-		grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_ADMINISTRADOR"));
+		grantedAuthorityList.add(new SimpleGrantedAuthority("ROLE_" + user.getTipo()));
 
-		return new UsernamePasswordAuthenticationToken(username, password, grantedAuthorityList);
+		final UserDetails principal = new CustomUserDetails(username, password, grantedAuthorityList, user);
+
+		return new UsernamePasswordAuthenticationToken(principal, password, grantedAuthorityList);
 	}
 
 	@Override
 	public boolean supports(final Class<?> authentication) {
 
 		return authentication.equals(UsernamePasswordAuthenticationToken.class);
+	}
+
+	public Usuario getAuthenticatedUser() {
+
+		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		final CustomUserDetails userDatils = (CustomUserDetails) authentication.getPrincipal();
+
+		return userDatils.getUsuario();
+
 	}
 
 }
